4bcd4e3f514b67340c2c15e9b850ce35fcfe38de
Lee Paludan <Leepaludan@gmail.com>
2019-01-07 13:30:13 +0100

    Most of the SML functions added

---
+++ DrawingTrees/DrawingTrees/Main.fsx
@@ -0,0 +1,13 @@
+﻿#load "Types.fsx"
+
+open DrawingTrees.Types
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let a = [[(1.0, 2.0); (3.0, 4.0)]; [(5.0, 6.0); (7.0, 8.0)];
+        [(9.0, 10.0); (11.0, 12.0); (13.0, 14.0)]]
+    let var = fitlistl a
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ DrawingTrees/DrawingTrees/Types.fsx
@@ -0,0 +1,50 @@
+﻿namespace DrawingTrees
+module Types =
+    type 'a Tree = Node of 'a * ('a Tree list)
+    type Extent = (float * float) list
+
+    let movetree (Node((label, x), subtrees), x' : float) =
+        Node((label, x+x'), subtrees)
+
+    let moveextent (e : Extent, x) = List.map (fun (p,q) -> (p+x,q+x)) e
+
+    let rec merge (ex : Extent * Extent) : Extent =
+        match ex with
+        | ([], qs) -> qs
+        | (ps, []) -> ps
+        | ((p,_)::ps, (_,q)::qs) -> (p,q) :: merge (ps, qs)
+
+    let mergelist es = 
+        List.fold (fun acc x ->
+                merge (acc, x)
+             ) [] es
+
+    let rmax (p : float, q : float) = if p > q then p else q
+
+    (* fit: Extent*Extent -> float  ) *)
+    let rec fit e =
+        match e with
+        | ([], _) | (_, []) -> 0.0
+        | ( ((_,p)::ps),((q,_)::qs) ) -> rmax(fit (ps,qs), p - q + 1.0)
+
+
+    let fitlistl es =
+        let rec fitlistl' es =
+            match es with
+            | (acc, []) -> []
+            | (acc, (e::ex)) -> let x = (fit (acc, e)) 
+                                x :: (fitlistl' ( (merge(acc,moveextent (e,x))),ex))
+        fitlistl' ([], es)
+
+    let fitlistr es =
+        let rec fitlistl' es =
+            match es with
+            | (acc, []) -> []
+            | (acc, (e::ex)) -> let x = (fit (acc, e)) 
+                                x :: (fitlistl' ( (merge(acc,moveextent (e,x))),ex))
+        fitlistl' (es, [])
+
+
+    let mean (x,y) = (x+y) / 2.0
+
+    let fitlist es = List.map map mean (zip (fitlistl es, fitlistr es))
